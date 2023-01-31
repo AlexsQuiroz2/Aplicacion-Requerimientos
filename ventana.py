0be4c8d0e3f48ce2105690aad06f01385bdbead8
alexs <alexsquiroz054@gmail.com>
2023-01-31 15:03:00 -0500

    creacion de input

--- ventana.py
+++ ventana.py
@@ -4,7 +4,7 @@ from tkinter import *
 class ventana (Frame):
 
     def __init__(self, master=None):
-        super().__init__(master, width=680, height=260)
+        super().__init__(master, width=1366, height=768)
         self.master = master
         self.pack()
         self.create_widgets()
@@ -12,10 +12,57 @@ class ventana (Frame):
     def fNuevo(self):
         pass
 
+    def fModificar(self):
+        pass
+
+    def fEliminar(self):
+        pass
+
     def create_widgets(self):
+        #---------------Primera seccino---------------
         frame1 = Frame(self, bg="#bfdaff")
-        frame1.place(x=0, width=93, height=259)
+        frame1.place(x=0, width=210, height=768)
 
         #creacion del bonton de Nuevo
         self.btnNuevo=Button(frame1, text="Nuevo", command=self.fNuevo, bg="blue", fg="white")
-        self.btnNuevo.place(x=5, y=50, width=80, height=30)
+        self.btnNuevo.place(x=45, y=30, width=110, height=50)
+
+        #creacion del bonton de Modificar
+        self.btnModificar=Button(frame1, text="Modificar", command=self.fNuevo, bg="blue", fg="white")
+        self.btnModificar.place(x=45, y=90, width=110, height=50)
+
+        #creacion del bonton de Eliminar
+        self.btnEliminar=Button(frame1, text="Eliminar", command=self.fNuevo, bg="blue", fg="white")
+        self.btnEliminar.place(x=45, y=150, width=110, height=50)
+
+        #---------------Segunda seccion---------------
+        frame2 = Frame(self, bg="#d3dde3")
+        frame2.place(x=215, y=0, width=390, height=768)
+
+        #entrada de datos
+        lbl = Label(frame2, text=("Llene los siguiente campos"))
+        lbl1.place(x=6, y=130)
+
+        lbl1 = Label(frame2, text="Id: ")
+        lbl1.place(x=6, y=130)
+
+        self.txtCampaña=Entry(frame2)
+        self.txtCampaña.place(x=6, y=150, width=350, height=50)
+
+        lbl2 = Label(frame2, text="Campaña: ")
+        lbl2.place(x=6, y=230)
+
+        self.txtSerie=Entry(frame2)
+        self.txtSerie.place(x=6, y=250, width=350, height=50)
+
+        lbl3 = Label(frame2, text="Serie: ")
+        lbl3.place(x=6, y=330)
+
+        self.txtCampaña=Entry(frame2)
+        self.txtCampaña.place(x=6, y=350, width=350, height=50)
+
+        lbl4 = Label(frame2, text="Hostname: ")
+        lbl4.place(x=6, y=430)
+
+        self.txtCampaña=Entry(frame2)
+        self.txtCampaña.place(x=6, y=450, width=350, height=50)
