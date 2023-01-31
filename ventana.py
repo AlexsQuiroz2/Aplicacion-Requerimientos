7cd9fd2a1de070e765f5885821027c21c05d7d26
alexs <alexsquiroz054@gmail.com>
2023-01-31 12:43:28 -0500

    creacion de boton n1

--- ventana.py
+++ ventana.py
@@ -9,5 +9,13 @@ class ventana (Frame):
         self.pack()
         self.create_widgets()
 
-    def create_widgets(self):
+    def fNuevo(self):
         pass
+
+    def create_widgets(self):
+        frame1 = Frame(self, bg="#bfdaff")
+        frame1.place(x=0, width=93, height=259)
+
+        #creacion del bonton de Nuevo
+        self.btnNuevo=Button(frame1, text="Nuevo", command=self.fNuevo, bg="blue", fg="white")
+        self.btnNuevo.place(x=5, y=50, width=80, height=30)
